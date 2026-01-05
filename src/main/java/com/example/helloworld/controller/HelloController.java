130bbc4049e451988d293611f6b2a7eac8d3d98f
HwangGwihwan <144326170+HwangGwihwan@users.noreply.github.com>
2026-01-05 18:10:52 +0900

    first commit

---
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@ResponseBody
+public class HelloController {
+	
+	@GetMapping("/")
+	public String hello() throws InterruptedException {
+		log.info("get request");
+		bar();
+		
+		return "HelloWorld";
+	}
+	
+	public void bar() throws InterruptedException {
+		Thread.sleep(5000);
+		log.info("end bar method");
+	}
+}
